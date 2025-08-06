c419fea8c5fe300f69f70018dc97fb4314f12f57
mateomramos <mateomramos1@gmail.com>
2025-08-06 02:54:57 -0400

    implement search feature with react useState and API calls

--- weather-app/src/weather.jsx
+++ weather-app/src/weather.jsx
@@ -6,31 +6,85 @@ import DateTimeDisplay from "./clock";
 
 function Weather ()  {
 
-    return (
 
-      <div className="web-app">
+  const [ status, setStatus ] = useState('Idle');
+  const [ location, setLocation ] = useState('New York City');
 
-        <div className='location-input'>
-          <input type="text" placeholder="Enter location" />
-          <button>Search</button>
-        </div>
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-        <div className='weather-info'>
-          <div className='forecast-info'>
-            <DateTimeDisplay />
+  useEffect(() => {
+    ApiCall();
+  }, []);
 
-          </div>
+  const ApiCall = async () => {
+    setStatus('Loadng...')
 
-          <div className='forecast'>
+    try {
+      const response = await fetch(    
+        `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&aqi=no&alerts=no`
+      );
 
-          </div>
+      if (!response.ok) throw new Error('Request failed');
 
-        </div>
-        
-          
+      const data = await response.json();
+      console.log('API Response: ', data);
+      setStatus('Success');
+
+    } catch (err) {
+      console.error('API Error: ', err)
+      setStatus('Error');
+    }
+  };
+
+  return (
+
+    <div className="web-app">
+
+      <div className='location-input'>
+        <input 
+          type="text" 
+          placeholder="Enter location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button onClick={ApiCall}>Search</button>
       </div>
 
-    );
+      <div className='weather-info'>
+        <div className='forecast-info'>
+          <DateTimeDisplay />
+
+          <div className='conditions'>
+            <h1>75° F</h1>
+          </div>
+
+          <div className='add-conditions'>
+            <div className='cond1'>
+              <p>Wind Speed</p>
+            </div>
+            <div className='cond2'>
+              <p>Humidity</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div className='forecast'>
+          <div className='graph'>
+
+          </div>
+
+          <div className='cards'>
+
+          </div>
+        </div>
+
+      </div>
+      
+        
+    </div>
+
+  );
 }
 
 export default Weather;
